7ffb8d72077f28643b0bb6b1b12cfca79386d2a5
Oshan96 <oshanmen@gmail.com>
2019-09-02 21:47:47 +0530

    initial springboot commit

---
+++ src/main/java/io/github/baamu/baamubackend/BaamuBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.baamu.baamubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaamuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaamuBackendApplication.class, args);
+    }
+
+}
